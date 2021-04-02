acb90f6f6eec2d3708807586db0b2e022158ab0e
Nimrod Gabbay <nimrod240597@gmail.com>
2021-04-02 13:47:07 +0300

    updated files

---
+++ milestone11/IFlightGearModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace milestone1
+{
+    interface IFlightGearModel: INotifyPropertyChanged
+    {
+        double SliderValue { set; get; }
+
+        void connect(string ip, int port);
+        //void disconnect();
+        void start(string path);
+        void moveSlider(double value);
+        void pause();
+        void resume();
+        void stop();
+    }
+}
