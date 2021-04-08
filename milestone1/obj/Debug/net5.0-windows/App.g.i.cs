160e7474df328d704fc89ae69df8b203be2b50fe
Nimrod Gabbay <nimrod240597@gmail.com>
2021-04-08 15:40:52 +0300

    merge

--- milestone1/obj/Debug/net5.0-windows/App.g.i.cs
+++ milestone1/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace milestone1 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/milestone1;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/milestone1;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
