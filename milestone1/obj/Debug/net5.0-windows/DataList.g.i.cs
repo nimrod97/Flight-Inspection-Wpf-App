ac912f18c1c0028b457ad545c0ac28db954d4982
Bar Tawil <bruriya.tawil@live.biu.ac.il>
2021-04-11 16:34:35 +0300

    Window

--- milestone1/obj/Debug/net5.0-windows/DataList.g.i.cs
+++ milestone1/obj/Debug/net5.0-windows/DataList.g.i.cs
@@ -61,7 +61,7 @@ namespace milestone1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/milestone1;component/datalist.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/milestone1;V1.0.0.0;component/datalist.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\DataList.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
