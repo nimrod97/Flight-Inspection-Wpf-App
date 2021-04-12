a47470bfcbbc26e333dde0188bb660d2a739c41d
NoaMiara <71879700+NoaMiara@users.noreply.github.com>
2021-04-12 17:40:19 +0300

    Add files via upload

---
+++ IFlightGearModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace milestone1
+{
+    interface IFlightGearModel: INotifyPropertyChanged
+    {
+        float Altitude { set; get; }
+        float AirSpeed { set; get; }
+        float HeadingDeg { set; get; }
+        float PitchDeg { set; get; }
+        float RollDeg { set; get; }
+        float YawDeg { set; get; }
+        float Aileron { set; get; }
+        float Elevator { set; get; }
+        float Rudder { set; get; }
+        float Throttle { set; get; }
+
+        double SliderValue { set; get; }
+        double SimulatorSpeed { set; get; }
+
+        string[] Properties { get; }
+
+        void connect(string ip, int port);
+        //void disconnect();
+        void start(string path);
+        void moveSlider(double value);
+        void pause();
+        void resume();
+        void stop();
+        void moveSimulatorSpeed(double value);
+
+        PlotModel PlotModel { get; set; }
+        public string CurrerntChoice { get; set; }
+
+/*        IList<DataPoint> PointsCurrentChoice { get; set; }
+*/    }
+}
